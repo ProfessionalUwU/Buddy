b6100cd7a7c6e2f8acee115a917b5e0b9e063899
Throndrache606 <58053153+Throndrache606@users.noreply.github.com>
2020-12-30 19:44:22 +0100

    Updated
    Stand 30.12.2020 19:44

--- log.js
+++ log.js
@@ -1,3 +1,16 @@
+var check = function() {
+	if (document.getElementById('passwort').value ==
+		  document.getElementById('passwortverify').value) {
+		  document.getElementById('message').style.color = 'green';
+		  document.getElementById('message').innerHTML = 'matching';
+		  document.getElementById("test").style.display = "block";
+	} else {
+			document.getElementById('message').style.color = 'red';
+		  document.getElementById('message').innerHTML = 'not matching';
+		  document.getElementById("test").style.display = "none";
+	}
+}
+
 $('.form').find('input, textarea').on('keyup blur focus', function(e) {
 	var $this = $(this),
 		label = $this.prev('label');
